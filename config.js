00b3de309659dc662f87a1cedd0ae503da8182b6
sasfandersoncosta-specA <sasf.andersoncosta@gmail.com>
2025-11-24 00:29:27 -0300

    err

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // IMPORTANTE: Este arquivo deve ser carregado no HTML ANTES de qualquer outro script.
 
 const API_BASE_URL = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost'
-    ? 'http://localhost:3001'  // Seu Backend Local
-    : 'https://projeto-girassol.onrender.com/'; 
-
+    ? 'http://localhost:3001' 
+    : 'https://projeto-girassol.onrender.com';
+Ação:
 console.log(`[Jano Config] Ambiente detectado: ${window.location.hostname}. API definida para: ${API_BASE_URL}`);
 
 // Função auxiliar para toast de erro global
